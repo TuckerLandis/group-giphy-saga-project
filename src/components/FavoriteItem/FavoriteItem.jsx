eef47f6bf511e57595adbd420fb87f0c0f2756b8
James Kelley <jkkelley86@gmail.com>
2021-06-17 15:39:32 -0500

    testing

--- src/components/FavoriteItem/FavoriteItem.jsx
+++ src/components/FavoriteItem/FavoriteItem.jsx
@@ -1,10 +1,14 @@
-function FavoriteItem() {
+// import { ImageSearch } from "@material-ui/icons";
+// import { useDispatch } from "react-redux";
 
-    return (
-        <h3>In FavoriteItem</h3>
-    )
-}
+// function FavoriteItem({images}) {
+//   return (
+//     <div>
+//       <h3>In FavoriteItem</h3>
+//         {/* <img src={images.url}></img> */}
+//         <button>Favorite</button>
+//     </div>
+//   );
+// }
 
-export default FavoriteItem;
-
-
+// export default FavoriteItem;
--- src/components/FavoriteList/FavoriteList.jsx
+++ src/components/FavoriteList/FavoriteList.jsx
@@ -1,15 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
 
 // components
-import FavoriteItem from '../FavoriteItem/FavoriteItem.jsx';
+// import FavoriteItem from "../FavoriteItem/FavoriteItem.jsx";
 
 function FavoriteList() {
+  const dispatch = useDispatch();
+  const favoritesList = useSelector((store) => store.favoritesList);
 
-    // use effect for fetch favorites - from DB
+  // Dispatch to get our Favorite Images from the redux store
+  const getFavoriteImages = () => {
+    dispatch({ type: "FETCH_FAVORITES" });
+  };
+  // use effect for fetch favorites - from DB
+  useEffect(() => {
+    getFavoriteImages();
+  }, []);
 
-    return (
-        <h3>In FavoriteList</h3>
-        // Map into FavoriteItem here
-    )
+  return (
+    <>
+      <h3>In FavoriteList</h3>
+      {/* // Map into FavoriteItem here */}
+      {/* {favoritesList.map((images, i) => {
+        <FavoriteItem key={images.id} images={images} />;
+      })} */}
+    </>
+  );
 }
 
 export default FavoriteList;
