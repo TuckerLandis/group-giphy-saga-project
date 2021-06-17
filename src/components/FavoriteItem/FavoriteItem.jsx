153e03bbb4ed52635c0b53274f677b9507a89279
James Kelley <jkkelley86@gmail.com>
2021-06-17 16:51:19 -0500

    select ready to go

--- src/components/FavoriteItem/FavoriteItem.jsx
+++ src/components/FavoriteItem/FavoriteItem.jsx
@@ -1,14 +1,49 @@
-// import { ImageSearch } from "@material-ui/icons";
-// import { useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
-// function FavoriteItem({images}) {
-//   return (
-//     <div>
-//       <h3>In FavoriteItem</h3>
-//         {/* <img src={images.url}></img> */}
-//         <button>Favorite</button>
-//     </div>
-//   );
-// }
+// Material-ui
+import { ImageSearch } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import Select from "@material-ui/core/Select";
 
-// export default FavoriteItem;
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 150,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+function FavoriteItem({ images }) {
+  // Below allows us to use useStyles from above
+  const classes = useStyles();
+  // Local State to hold Select Value state
+  const [selectState, setSelectState] = useState("");
+  // Handle Change function
+  const handleChange = (event) => {
+    setSelectState(event.target.value);
+  };
+  return (
+    <div>
+      <h3>In FavoriteItem</h3>
+      <img src={images.url} alt="searched GIF"></img>
+      <FormControl className={classes.formControl} required>
+        <InputLabel id="demo-simple-select-label">Set Favorite Category</InputLabel>
+        <Select value={selectState} onChange={handleChange}>
+          <MenuItem value={"funny"}>Funny</MenuItem>
+          <MenuItem value={"cohort"}>Cohort</MenuItem>
+          <MenuItem value={"cartoon"}>Cartoon</MenuItem>
+          <MenuItem value={"nsfw"}>NSFW</MenuItem>
+          <MenuItem value={"meme"}>Meme</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+
+export default FavoriteItem;
--- src/components/FavoriteList/FavoriteList.jsx
+++ src/components/FavoriteList/FavoriteList.jsx
@@ -1,28 +1,21 @@
 import { useDispatch, useSelector } from "react-redux";
 
 // components
-// import FavoriteItem from "../FavoriteItem/FavoriteItem.jsx";
+import FavoriteItem from "../FavoriteItem/FavoriteItem.jsx";
 
 function FavoriteList() {
   const dispatch = useDispatch();
   const favoritesList = useSelector((store) => store.favoritesList);
 
-  // Dispatch to get our Favorite Images from the redux store
-  const getFavoriteImages = () => {
-    dispatch({ type: "FETCH_FAVORITES" });
-  };
-  // use effect for fetch favorites - from DB
-//   useEffect(() => {
-//     getFavoriteImages();
-//   }, []);
+
   console.log(favoritesList)
   return (
     <>
       <h3>In FavoriteList</h3>
       {/* // Map into FavoriteItem here */}
-      {/* {favoritesList.map((images, i) => {
-        <FavoriteItem key={images.id} images={images} />;
-      })} */}
+      {favoritesList.map((imageData, index) => {
+        <FavoriteItem key={index} imageData={imageData} />;
+      })}
     </>
   );
 }
